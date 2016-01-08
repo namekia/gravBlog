94d5d7268d06a778e9399eff94e1f3c4517768a2
Gregory Chambers <greg@gChambers.com>
2016-01-08 11:57:54 -0600

    refresher

--- ___github-refresher.php
+++ ___github-refresher.php
@@ -3,7 +3,7 @@ date_default_timezone_set('America/Chicago');
 ignore_user_abort(true);
 set_time_limit(0);
 
-$repo          = 'https://github.com/namekia/gravBlog';
+$repo          = '~/public_html/gravBlog';
 $branch        = 'master';
 $output        = array();
 
